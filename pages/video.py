ba5b4113d80ff5eba8e130ec9eef2ee19ebf7738
Mert Ayvazoglu <mayvazog@mail.uni-mannheim.de>
2024-04-03 21:15:21 +0200

    add video

--- pages/video.py
+++ pages/video.py
@@ -1,9 +1,11 @@
 import streamlit as st
 import os
 
-print(os.getcwd())
-print("\n")
-video_file = open('/data/vid.mp4', 'rb')
+# Obtain the current working directory
+current_path = os.getcwd()
+
+# Proceed with your code to read and display the video file
+video_file = open('data/lecture_6.mp4', 'rb')
 video_bytes = video_file.read()
 
 st.video(video_bytes, format="video/mp4", start_time=3)
